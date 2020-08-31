b2b7f6b41f7718401fb893825bc4bd5cc6332181
Alba Gustems <albagustems@gmail.com>
2020-08-31 23:34:11 +0200

    Room model created. Room CRUD finished and working, pending Google maps implementation. Added get userprofile/:id route

---
+++ models/room-model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+  owner:{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  property: {
+    type: String,
+    enum : ['house','flat','other'],
+    required: true
+  },
+  location: {
+    type: Object,
+  },
+  images: {
+    type: [String],
+    default: ['https://res.cloudinary.com/agustems/image/upload/v1598881434/roomer/no-image_klmdah.png']
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  size: {
+    type: String,
+    enum: ['individual', 'double'],
+    required: true
+  },
+  availability: Date,
+  ammenities: {
+    living: Boolean,
+    internet: Boolean,
+    parking: Boolean,
+    balcony: Boolean,
+    garden: Boolean,
+    dacces: Boolean
+  },
+  flatmates: {
+    type: [Number],
+    default: [0,0]
+  },
+  bedrooms: {
+    type: Number,
+    required: true
+  },
+  bathrooms: {
+    type: Number,
+    required: true 
+  },
+  pets: Boolean,
+  smokers: Boolean,
+  tolerance: {
+    guys: Boolean,
+    girls: Boolean,
+    couples: Boolean,
+    smokers: Boolean,
+    students: Boolean,
+    pets: Boolean
+  },
+  title: String,
+  description: String
+})
+
+const Room = mongoose.model('Room', roomSchema)
+module.exports = Room
