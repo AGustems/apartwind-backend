bdf176574dbdf161fa5975c2af7eba6532a6cd28
Alba Gustems <albagustems@gmail.com>
2020-09-06 16:04:44 +0200

    User and Room model validation and default values specified

--- models/room-model.js
+++ models/room-model.js
@@ -2,66 +2,135 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const roomSchema = new Schema({
-  owner:{
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  property: {
-    type: String,
-    enum : ['House','Flat','Other'],
-    required: true
-  },
-  location: {
-    type: Object,
-  },
-  images: {
-    type: [String],
-    default: ['https://res.cloudinary.com/agustems/image/upload/v1598881434/roomer/no-image_klmdah.png']
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  size: {
-    type: String,
-    enum: ['individual', 'double'],
-    required: true
-  },
-  availability: Date,
-  amenities: {
-    living: Boolean,
-    internet: Boolean,
-    parking: Boolean,
-    balcony: Boolean,
-    garden: Boolean,
-    dacces: Boolean
-  },
-  flatmates: {
-    type: [Number],
-    default: [0,0]
-  },
-  bedrooms: {
-    type: Number,
-    required: true
-  },
-  bathrooms: {
-    type: Number,
-    required: true 
-  },
-  pets: Boolean,
-  smokers: Boolean,
-  tolerance: {
-    guys: Boolean,
-    girls: Boolean,
-    couples: Boolean,
-    smokers: Boolean,
-    students: Boolean,
-    pets: Boolean
-  },
-  title: String,
-  description: String
-})
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'You need to be logged in to post an advert']
+    },
+    property: {
+        type: String,
+        enum: [
+            'House', 'Flat', 'Other'
+        ],
+        required: [true, 'You need to specify the property type']
+    },
+    location: {
+        lat: {
+            type: String,
+            required: [true, 'You need to specify the property location']
+        },
+        lng: {
+            type: String,
+            required: [true, 'You need to specify the property location']
+        },
+        direction: {
+            type: String,
+            required: [true, 'You need to specify the property location']
+        }
+    },
+    images: {
+        type: [String],
+        match: [
+            /^(ftp|http|https):\/\/[^ "]+$/, 'Invalid url'
+        ],
+        default: ['https://res.cloudinary.com/agustems/image/upload/v1598881434/roomer/no-image_klm' +
+                'dah.png']
+    },
+    price: {
+        type: Number,
+        required: [true, 'You need to specify the monthly rent']
+    },
+    size: {
+        type: String,
+        enum: [
+            'individual', 'double'
+        ],
+        required: [true, 'You need to specigy the room size']
+    },
+    availability: Date,
+    amenities: {
+        living: {
+            type: Boolean,
+            default: false
+        },
+        internet: {
+            type: Boolean,
+            default: false
+        },
+        parking: {
+            type: Boolean,
+            default: false
+        },
+        balcony: {
+            type: Boolean,
+            default: false
+        },
+        garden: {
+            type: Boolean,
+            default: false
+        },
+        dacces: {
+            type: Boolean,
+            default: false
+        }
+    },
+    flatmates: {
+        type: [Number],
+        default: [0, 0]
+    },
+    bedrooms: {
+        type: Number,
+        required: true,
+        match: [/^(?:[1-9]\d+|[1-9])$/gm, 'There has to be at least one bedroom']
+    },
+    bathrooms: {
+        type: Number,
+        required: true,
+        match: [/^(?:[1-9]\d+|[1-9])$/gm, 'There has to be at least one bathroom']
+    },
+    pets: {
+        type: Boolean,
+        default: false
+    },
+    smokers: {
+        type: Boolean,
+        default: false
+    },
+    tolerance: {
+        guys: {
+            type: Boolean,
+            default: false
+        },
+        girls: {
+            type: Boolean,
+            default: false
+        },
+        couples: {
+            type: Boolean,
+            default: false
+        },
+        smokers: {
+            type: Boolean,
+            default: false
+        },
+        students: {
+            type: Boolean,
+            default: false
+        },
+        pets: {
+            type: Boolean,
+            default: false
+        }
+    },
+    title: {
+        type: String,
+        default: 'No title was provided'
+    },
+    description: {
+        type: String,
+        default: 'No description was provided'
+    }
+}, {timestamps: true})
 
 const Room = mongoose.model('Room', roomSchema)
 module.exports = Room
--- models/user-model.js
+++ models/user-model.js
@@ -4,30 +4,68 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'The name is required']
     },
     surname: {
         type: String,
-        required: true
+        required: [true, 'The surname is required']
     },
     email: {
         type: String,
-        required: true
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, 'Invalid email address'],
+        required: [true, 'The email is required'],
     },
     password: {
         type: String,
         required: true
     },
-    occupation: String,
+    occupation: {
+        type: String,
+        default: 'Not Specified'
+    },
     imageUrl: {
         type: String,
+        match: [
+            /^(ftp|http|https):\/\/[^ "]+$/, 'Invalid url'
+        ],
         default: 'https://res.cloudinary.com/agustems/image/upload/v1598692094/roomer/newUser_pmu8' +
                 'bv.png'
     },
-    age: Number,
-    description: String,
+    age: {
+        type: Number,
+        default: 0
+    },
+    description: {
+        type: String,
+        default: "There isn't any description for this user yet."
+    },
     characteristics: [String],
-    socials: Object,
+    socials: {
+        facebook: {
+            type: String,
+            match: [
+                /^(https?:\/\/){0,1}(www\.){0,1}facebook\.com/gm, 'Invalid Facebook address'
+            ],
+            default: 'No facebook account information provided'
+        },
+        twitter: {
+            type: String,
+            match: [
+                /^(https?:\/\/){0,1}(www\.){0,1}twitter\.com/gm, 'Invalid Facebook address'
+            ],
+            default: 'No twitter account information provided'
+        },
+        instagram: {
+            type: String,
+            match: [
+                /^(https?:\/\/){0,1}(www\.){0,1}instagram\.com/gm, 'Invalid Instagram address'
+            ],
+            default: 'No instagram account information provided'
+        }
+    },
     favourites: [
         {
             type: Schema.Types.ObjectId,
@@ -35,11 +73,11 @@ const userSchema = new Schema({
         }
     ],
     adverts: [
-      {
-          type: Schema.Types.ObjectId,
-          ref: 'Room'
-      }
-  ],
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Room'
+        }
+    ]
 }, {timestamps: true})
 
 const User = mongoose.model('User', userSchema)
