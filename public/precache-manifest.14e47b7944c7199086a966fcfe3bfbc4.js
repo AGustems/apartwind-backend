e45777222f1965869652b0736e46191dac778212
Alba Gustems <albagustems@gmail.com>
2020-09-12 16:16:05 +0200

    Deploy Test

---
+++ public/precache-manifest.14e47b7944c7199086a966fcfe3bfbc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd48cf88ce22dc585edc845910a4ba62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f474a0ff79fba374c1",
+    "url": "/static/css/main.aa8183e0.chunk.css"
+  },
+  {
+    "revision": "fc48f116cad29ea57870",
+    "url": "/static/js/2.0fafc7a0.chunk.js"
+  },
+  {
+    "revision": "2badc1b209c5a040c707dcd720b10f8a",
+    "url": "/static/js/2.0fafc7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f474a0ff79fba374c1",
+    "url": "/static/js/main.0eb1feda.chunk.js"
+  },
+  {
+    "revision": "e06b819808dd956ceee6",
+    "url": "/static/js/runtime-main.83717362.js"
+  }
+]);
